8e3a7bc89046f6bcbeebc1e6fe199bfbb05446b1
home-mia <ydjvchslf@gmail.com>
2023-11-29 15:03:08 +0900

    검색화면 - 텍스트왓쳐 적용

--- app/src/main/java/com/example/searchingimage/search/SearchFragment.kt
+++ app/src/main/java/com/example/searchingimage/search/SearchFragment.kt
@@ -1,9 +1,13 @@
 package com.example.searchingimage.search
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -12,12 +16,17 @@ import com.example.searchingimage.R
 import com.example.searchingimage.databinding.FragmentSearchBinding
 import com.example.searchingimage.util.AppDebug
 import com.example.searchingimage.util.fragTitle
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.subjects.PublishSubject
+import java.util.concurrent.TimeUnit
 
 class SearchFragment : Fragment() {
 
     private val logTag = SearchFragment::class.simpleName
     private lateinit var binding: FragmentSearchBinding
     private val searchViewModel: SearchViewModel by activityViewModels()
+
+    val textChange: PublishSubject<String> = PublishSubject.create()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -43,5 +52,29 @@ class SearchFragment : Fragment() {
         binding.bookmarkBtn.setOnClickListener {
             Navigation.findNavController(binding.root).navigate(SearchFragmentDirections.actionSearchFragmentToBookmarkFragment())
         }
+        // editText 포커싱, 검색어 rxJava
+        binding.editText.apply {
+            //requestFocus()
+            addTextChangedListener(object: TextWatcher{
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    textChange.onNext(s.toString())
+                }
+                override fun afterTextChanged(s: Editable?) {}
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            })
+
+            textChange
+                .debounce(1, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnNext { keyword ->
+                    if (keyword.isNotEmpty() && keyword.isNotBlank()) {
+                        AppDebug.d(logTag, "keyword => $keyword")
+                    } else {
+                        AppDebug.d(logTag, "아무것도 안적었슈")
+                    }
+                }
+                .subscribe()
+        }
     }
+
 }
